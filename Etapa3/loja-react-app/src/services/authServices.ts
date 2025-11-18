9c86dbd185e83a63a6f5386dbfa220bd92ca4c8d
Anna Eduarda <ratue.fan@gmail.com>
2025-11-18 09:52:46 -0300

    Extrai dados do usuário do token

--- Etapa3/loja-react-app/src/services/authServices.ts
+++ Etapa3/loja-react-app/src/services/authServices.ts
@@ -1,7 +1,7 @@
 import Constants from 'expo-constants'; // novo
 
 // const API_URL = Config.API_URL;
-// const API_URL = 'http://10.81.205.24:5000';
+// const API_URL = 'http://10.81.205.50:5000';
 const { apiUrl } = Constants.expoConfig?.extra || {}; // novo
 
 export async function fakeLogin(email: string, password: string): Promise<string> {
@@ -34,15 +34,15 @@ export async function requestLogin(email: string, password: string): Promise<str
     }
 }
 
-export async function requestRegister(name: string,email: string, password: string): Promise<string> {
+// dupliquei o requestLogin, renomeie e inclui o name nos argumentos.
+export async function requestRegister(name: string, email: string, password: string): Promise<string> {
     try {
-        // alterado
-        const response = await fetch(`${apiUrl}/api/users`, {
+        const response = await fetch(`${apiUrl}/api/users`, { // modifiquei o endpoint
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({name ,email, password}),
+            body: JSON.stringify({name, email, password}), // inclui o name
         });
         const data = await response.json();
         const jwt = data.accessToken;
@@ -56,6 +56,17 @@ export async function requestRegister(name: string,email: string, password: stri
     }
 }
 
-
-
-
+export async function getTokenData(token: string | null): Promise<any[]> {
+    try {
+        if (!token) {
+            return [];
+        }
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(atob(base64));
+        return payload;
+    } catch (error) {
+        console.error(error);
+        return Promise.reject('Token inválido.');
+    }
+}
